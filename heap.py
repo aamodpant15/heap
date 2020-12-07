f5b97c8293652b27cf214316f9761057e467e11d
Aamod Pant Balekundri <64889283+aamodpant15@users.noreply.github.com>
2020-12-07 10:57:26 +0530

    Update heap.py

--- heap.py
+++ heap.py
@@ -18,12 +18,6 @@ class MaxHeap:
         A = self.heap
         l = self.get_left(i)
         r = self.get_right(i)
-    
-        # print("\n        i=" + str(i))
-        # print("        l = " + str(l))
-        # print("        r = " + str(r))
-        # print("        size = " + str(self.get_heap_size()))
-        # print('\n')
         
         if l < self.get_heap_size() and A[l] > A[i]:
             # print("        Comparing "+ A[l] + ", "+  A[i])
@@ -44,7 +38,4 @@ class MaxHeap:
     def build_heap(self):
         # A = self.heap
         for i in range(int(self.heap_size/2),-1,-1):
-            # print("i=" + str(i))
             self.heapify(i)
-
-####################################################################################
