e3472b1739227e023de4583ac7a6e877715e7b13
Aamod Pant Balekundri <64889283+aamodpant15@users.noreply.github.com>
2020-12-08 14:49:41 +0530

    Update heapsort.py
    bug fix

--- heapsort.py
+++ heapsort.py
@@ -1,7 +1,7 @@
 from heap import MaxHeap
 
-def heapsort(A):
-    wh = MaxHeap(A) #word heap
+def heapsort(A, greater):
+    wh = MaxHeap(A, greater)
     for i in range(len(A)-1, 0, -1):
         temp = A[i]
         A[i] = A[0]
